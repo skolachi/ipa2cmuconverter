7723ab484cf3ee86fb727758cec9f0d7b9645d5d
skolachi <sudheer.kpg08@gmail.com>
2022-03-06 07:12:25 +0000

    Change converter implementation;Add vowel length diacritic to map;Add some dictionary files to test

--- ipa_to_cmu.py
+++ ipa_to_cmu.py
@@ -3,19 +3,21 @@
 # define the phone mapping in ipa2cmu.map and read this into a dictionary 
 ipa2cmu = dict([tuple(f.strip().split('\t')) for f in open('ipa2cmu.map')]) 
 
-print(ipa2cmu)
+#print(ipa2cmu)
 
 def convert(text):
 	cmu = []
-	for i in range(len(text)):
-		if i < len(text) - 1:
-			if text[i:i+2] in ipa2cmu.keys():
-				cmu.append(ipa2cmu[text[i:i+2]])
-			else:
-				cmu.append(ipa2cmu[text[i]])
-		else:
+	i = 0
+	while i < len(text):
+		if i < len(text) - 1 and text[i:i+2] in ipa2cmu.keys():
+			cmu.append(ipa2cmu[text[i:i+2]])
+			i += 1
+		elif text[i] in ipa2cmu.keys():
 			cmu.append(ipa2cmu[text[i]])
+		else:
+			cmu.append(text[i])
+		i += 1
 
 	return cmu
 
-print(convert('kiwi'))
+print(convert('kiwi dʒuːs'))
